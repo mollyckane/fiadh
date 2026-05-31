794eaedbc67b80407964a8a0f09c043a92f2ec22
emsie <mollyckanework@gmail.com>
2026-05-31 15:30:28 +0100

    use bcrypt + jwt, create routes
    - bcrypt to hash passwords
    - jwt tokens given to users
    - register + login routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,70 @@
 const express = require('express');
 const router = express.Router();
+//bcrypt for password hashing - hash passwords before storing in database
+const bcrypt = require('bcryptjs');
+//jsonwebtoken for creating and verifying JWT tokens: each user gets a token upon login, which they can use to authenticate future requests
+const jwt = require('jsonwebtoken');
+//import database connection
+const db = require('../config/db');
 
-// TODO: add routes here
+//---- ROUTES -----
+// Register route - create new user
+router.post('/register', async (req, res) => {
+    //pull name, email, password from request body
+    const { name, email, password } = req.body;
+
+    //try-catch for error handling
+    try {
+        //hash password before saving it, value of 10 is the salt rounds (higher is more secure but slower)
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        //SQL query to insert new user into database
+        const sql = 'INSERT INTO users (name, email, password) VALUES (?, ?, ?)';
+
+        //run query
+        //prevent SQL injection attacks by using parameterized queries (the ? placeholders) instead of directly inserting user input into the query string
+        db.query(sql, [name, email, hashedPassword], (err, result) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+            //if successful:
+            res.status(201).json({ message: 'User registered successfully.'});       
+        });                
+    }
+    catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+// Login route - authenticate user and return JWT token
+router.post('/login', (req, res) => {
+    //pull email and password from request body
+    const { email, password } = req.body;
+
+    //search database for user with this email
+    const sql = 'SELECT * FROM users WHERE email = ?';
+
+    db.query(sql, [email], async (err, results) => {
+        if(err) return res.status(500).json({ error: err.message});
+
+        //if no user found with this email, return error 401
+        if (results.length === 0) return res.status(401).json({ error: 'User not found'});
+
+        //grab first (and only) matching user from results
+        const user = results[0];
+
+        //compare provided password with hashed password in database
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        //if passwords don't match, return error 401
+        if (!isMatch) return res.status(401).json({ error: 'Incorrect password' });
+
+        //if passwords match, create JWT token -  expires in one day
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+
+        //return token to client - they will use this token to authenticate future requests
+        res.json({ token });
+    });
+})
 
 module.exports = router;
