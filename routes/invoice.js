6d33efcb0305f267fa8bf9852b0f95d41b0b785d
emsie <mollyckanework@gmail.com>
2026-06-03 22:33:51 +0100

    create home page with register and login sections
    - beginning of front end syling: homepage - register - login

--- routes/invoice.js
+++ routes/invoice.js
@@ -3,4 +3,12 @@ const router = express.Router();
 
 // TODO: add routes here
 
+//import auth middleware to protect routes
+const verifyToken = require('../middleware/auth');
+
+//TEST protected route - verifyToken runs before callback
+router.get('/', verifyToken, (req, res) => { 
+    res.json({ message: `Hello user ${req.userId}, this is a protected route!`, userId: req.userId });
+});
+
 module.exports = router;
