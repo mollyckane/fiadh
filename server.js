2491f04dcfca32fe2feecaffe353ebb1f478d6c7
mollyckane <mollyckanework@gmail.com>
2026-07-14 17:06:11 +0100

    altered to work with testing

--- server.js
+++ server.js
@@ -5,10 +5,9 @@ const app = express();
 
 app.use(express.json());
 
-//fronted
+//frontend
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/libs', express.static(path.join(__dirname, 'node_modules')));
-
 
 //routes
 const authRoutes = require('./routes/auth');
@@ -31,7 +30,11 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+if(require.main === module) {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+module.exports = app;
